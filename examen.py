cf3789054a0f29954f862f09f34d405c4a642df9
johnsalazr <johnsalazark@gmail.com>
2023-02-20 07:52:52 -0500

    John

---
+++ examen.py
@@ -0,0 +1,23 @@
+#variable = 5
+#variable_uno = float(variable)
+#varible_dos = str(variable)
+
+#print (variable, variable_uno, varible_dos)
+
+#sumar_datos=  (variable, variable_uno, varible_dos)
+#print(sumar_datos)
+
+#print (type (varible_dos))
+#print (type (variable_uno))
+#print (type (variable))
+
+#tipos de datos
+variable = "hi"
+variable2 = 1
+variable3 = 20
+variable5 = 1j
+variable5 = ["cantar","jugar","estudiar"] #lista
+variable6 = ("cantar","jugar","estudiar") #tupla
+carro ={"color": "rojo","velocidad": 300, "modelo": "gtr234"} #dict
+print (carro)
+
